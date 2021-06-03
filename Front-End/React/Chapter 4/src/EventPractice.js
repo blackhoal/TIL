25091aa13f8c71ad8b297f23a4598827cd83de97
blackhoal <48504392+blackhoal@users.noreply.github.com>
2021-06-03 09:59:18 +0900

    Update EventPractice.js

--- Front-End/React/Chapter 4/src/EventPractice.js
+++ Front-End/React/Chapter 4/src/EventPractice.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 
-class EventPractice extends Component {
+// 4-2-1 컴포넌트 생성
+class EventPractice_4_2_1 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>이벤트 연습</h1>
+            </div>
+        );
+    }
+}
 
+// 4-2-2 onChange 이벤트 핸들링
+class EventPractice_4_2_2 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>이벤트 연습</h1>
+                <input
+                    type = "text"
+                    name = "message"
+                    placeholder = "아무거나 입력하세요."
+                    onChange = {
+                        (e) => {
+                            console.log(e.target.value);
+                        }
+                    }
+                />
+            </div>
+        );
+    }
+}
+
+// 4-2-2-3 버튼을 클릭 시 comment 값을 공백으로 설정하며 메시지 박스 출력
+class EventPractice_4_2_2_3 extends Component {
     state = {
         message : ''
     }
