48fe9c3ba88bb9cf00b0feff8bc3d8a01f030b4d
blackhoal <48504392+blackhoal@users.noreply.github.com>
2021-06-03 09:51:25 +0900

    Create App.js

---
+++ Front-End/React/Chapter 4/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EventPractice from './EventPractice';
+
+class App extends Component {
+  render() {
+    return (
+      <EventPractice/>
+    );
+  }
+}
+
+export default App;
